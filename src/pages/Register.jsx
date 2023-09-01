8678c912e7a9d3275efab04ff9b7f7a97ef62c32
Salih Can <bozkurtyazilim@hotmail.com>
2023-09-01 23:04:51 +0300

    test commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -21,6 +21,8 @@ const Register = () => {
       });
   }
 
+  //TEST
+
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-500 to-blue-500">
